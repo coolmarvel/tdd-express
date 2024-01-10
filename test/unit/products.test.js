1229040b3e81aea9dce85ad302f8d503cd5c5b18
coolmarvel <marvel97@naver.com>
2024-01-11 08:34:10 +0900

    unit test with product controller

--- test/unit/products.test.js
+++ test/unit/products.test.js
@@ -75,4 +75,159 @@ describe("Product Controller (GET)", () => {
 
     expect(Product.find).toHaveBeenCalledWith({});
   });
+
+  it("should return 200 response", async () => {
+    await productController.getProducts(req, res, next);
+
+    expect(res.statusCode).toBe(200);
+    expect(res._isEndCalled).toBeTruthy();
+  });
+
+  it("should return json body in response", async () => {
+    Product.find.mockReturnValue(allProducts);
+
+    await productController.getProducts(req, res, next);
+
+    expect(res._getJSONData()).toStrictEqual(allProducts);
+  });
+
+  it("should handle errors", async () => {
+    const errorMessage = { message: "Error finding product data" };
+    const rejectedPromise = Promise.reject(errorMessage);
+
+    Product.find.mockReturnValue(rejectedPromise);
+
+    await productController.getProducts(req, res, next);
+
+    expect(next).toHaveBeenCalledWith(errorMessage);
+  });
+});
+
+describe("Product Controller (GET) [GetById]", () => {
+  it("should have a getProductById", () => {
+    expect(typeof productController.getProductById).toBe("function");
+  });
+
+  it("should call Product.findById", async () => {
+    req.params.productId = productId;
+
+    await productController.getProductById(req, res, next);
+
+    expect(Product.findById).toBeCalledWith(productId);
+  });
+
+  it("should return json body and response code 200", async () => {
+    Product.findById.mockReturnValue(newProduct);
+
+    await productController.getProductById(req, res, next);
+
+    expect(res.statusCode).toBe(200);
+    expect(res._getJSONData()).toStrictEqual(newProduct);
+    expect(res._isEndCalled()).toBeTruthy();
+  });
+
+  it("should return 404 when item deosn't exist", async () => {
+    Product.findById.mockReturnValue(null);
+
+    await productController.getProductById(req, res, next);
+
+    expect(res.statusCode).toBe(404);
+    expect(res._isEndCalled()).toBeTruthy();
+  });
+
+  it("should handle errors", async () => {
+    const errorMessage = { message: "error" };
+    const rejectedPromise = Promise.reject(errorMessage);
+
+    Product.findById.mockReturnValue(rejectedPromise);
+
+    await productController.getProductById(req, res, next);
+
+    expect(next).toHaveBeenCalledWith(errorMessage);
+  });
+});
+
+describe("Product Controller (UPDATE)", () => {
+  it("should have an updateProduct function", () => {
+    expect(typeof productController.updateProduct).toBe("function");
+  });
+
+  it("should call Product.findByIdAndUpdate", async () => {
+    req.params.productId = productId;
+    req.body = updatedProduct;
+
+    await productController.updateProduct(req, res, next);
+
+    expect(Product.findByIdAndUpdate).toHaveBeenCalledWith(productId, updatedProduct, { new: true });
+  });
+
+  it("should return json body and response code 200", async () => {
+    req.params.productId = productId;
+    req.body = updatedProduct;
+
+    Product.findByIdAndUpdate.mockReturnValue(updatedProduct);
+
+    await productController.updateProduct(req, res, next);
+
+    expect(res.statusCode).toBe(200);
+    expect(res._isEndCalled()).toBeTruthy();
+    expect(res._getJSONData()).toStrictEqual(updatedProduct);
+  });
+
+  it("should handle 404 when item doesn't exist", async () => {
+    Product.findByIdAndUpdate.mockReturnValue(null);
+
+    await productController.updateProduct(req, res, next);
+
+    expect(res.statusCode).toBe(404);
+    expect(res._isEndCalled()).toBeTruthy();
+  });
+
+  it("should handle errors", async () => {
+    const errorMessage = { message: "Error" };
+    const rejectedPromise = Promise.reject(errorMessage);
+
+    Product.findByIdAndUpdate.mockReturnValue(rejectedPromise);
+
+    await productController.updateProduct(req, res, next);
+
+    expect(next).toHaveBeenCalledWith(errorMessage);
+  });
+});
+
+describe("Product Controller (DELETE)", () => {
+  it("should have a deleteProduct function", () => {
+    expect(typeof productController.deleteProduct).toBe("function");
+  });
+
+  it("should call Product.findByIdAndDelete", async () => {
+    req.params.productId = productId;
+
+    await productController.deleteProduct(req, res, next);
+
+    expect(Product.findByIdAndDelete).toBeCalledWith(productId);
+  });
+
+  it("should return 200 response", async () => {
+    const deletedProduct = { name: "deletedProduct", description: "it is deleted" };
+
+    Product.findByIdAndDelete.mockReturnValue(deletedProduct);
+
+    await productController.deleteProduct(req, res, next);
+
+    expect(res.statusCode).toBe(200);
+    expect(res._getJSONData()).toStrictEqual(deletedProduct);
+    expect(res._isEndCalled()).toBeTruthy();
+  });
+
+  it("should handle errors", async () => {
+    const errorMessage = { message: "Error deleting" };
+    const rejectedPromise = Promise.reject(errorMessage);
+
+    Product.findByIdAndDelete.mockReturnValue(rejectedPromise);
+
+    await productController.deleteProduct(req, res, next);
+
+    expect(next).toHaveBeenCalledWith(errorMessage);
+  });
 });
